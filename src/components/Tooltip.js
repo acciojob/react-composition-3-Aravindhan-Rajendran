e4bcee2961da7d2170568753da394e07c3f9457a
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-28 17:07:55 +0530

    Create Tooltip.js

---
+++ src/components/Tooltip.js
@@ -0,0 +1,9 @@
+const Tooltip=()=>{
+  <tooltip title="This is a tooltip">
+  <button type="button">Hover over me
+  </tooltip>
+  <tooltip title="This is a another tooltip">
+  <button type="button">Hover over me to see another tooltip
+  </tooltip>
+
+}
