1d7064e913a6ca5424bc1434949c4c475ed8c826
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-28 17:10:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
 import React from "react";
+import Tooltip from "./Tooltip";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-
+      <Tooltip text="This is a tooltip">
+        <button type="button">Hover over me</button>
+      </Tooltip>
+      <Tooltip text="This is another tooltip">
+        <button type="button">Hover over me to see another tooltip</button>
+      </Tooltip>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
