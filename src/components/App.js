b1994a5f64e1f41ec3643111e83cb273e1a9b877
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-29 21:40:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,18 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-      <h2 className="tooltip"><Tooltip tooltipId="firstTooltip" text="This is a tooltip for the first div">
+      <h2 className="tooltip">
+    <div>
+    <Tooltip tooltipId="firstTooltip" text="This is a tooltip for the first div">
         <h2>Hover over this heading</h2>
       </Tooltip>
+    </div>
     </h2>
       <p className="tooltip"><Tooltip tooltipId="secondTooltip" text="This is another tooltip for the second div">
+    <div>
         <p>Hover over this paragraph to see another tooltip</p>
       </Tooltip>
+    </div>
     </p>
     </div>
   );
