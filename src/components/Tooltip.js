4c8aab4c917eabc0162e04d81cd6dca2a23da96c
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-29 20:59:55 +0530

    Update Tooltip.js

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 
-const Tooltip = ({ text, children }) => {
+const Tooltip = ({ text, children, tooltipId }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   return (
     <div 
+      id={tooltipId}
       className="tooltip" 
       onMouseEnter={() => setIsVisible(true)}
       onMouseLeave={() => setIsVisible(false)}
